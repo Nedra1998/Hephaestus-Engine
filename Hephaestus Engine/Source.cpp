673caf338856e2b5db718acbb350298d32fe1fb6
Arden Rasmussen <ardenisthebest@gmail.com>
2016-04-02 11:19:32 -0700

    Incorporated Glut, and Attenpted to create window. Must define argc, argv, and RenderSceneCB. Downloading tutorial for help.

--- Hephaestus Engine/Source.cpp
+++ Hephaestus Engine/Source.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include "Libraries.h"
 #include "Consolas.h"
 using namespace std;
 void main() {
 	Logging::Initilize("Hephaestus Engine");
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1920, 1080);
+	glutCreateWindow("Hephaestus Engine");
+	glutDisplayFunc(RenderSceneCB);
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glutMainLoop();
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
 	Logging::Terminate();
 }
